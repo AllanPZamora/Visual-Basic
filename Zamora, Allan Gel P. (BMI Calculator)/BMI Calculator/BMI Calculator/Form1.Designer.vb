1352f9a1071775bf669b1da0994c44c850a321db
Allan Zamora <gelgelacc1@gmail.com>
2023-09-29 01:36:31 +0800

    bmi

---
+++ Zamora, Allan Gel P. (BMI Calculator)/BMI Calculator/BMI Calculator/Form1.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.wText = New System.Windows.Forms.TextBox()
+        Me.hText = New System.Windows.Forms.TextBox()
+        Me.BMIText = New System.Windows.Forms.TextBox()
+        Me.BMICText = New System.Windows.Forms.TextBox()
+        Me.CLEAR = New System.Windows.Forms.Button()
+        Me.btnadd = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(58, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(251, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "BMI CALCULATOR"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label2.Location = New System.Drawing.Point(12, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Weight"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 124)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Height"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 151)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(32, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "BMI"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(12, 180)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(95, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "BMI Category"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(114, 180)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(12, 17)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = ":"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(114, 151)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(12, 17)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = ":"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(114, 124)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(12, 17)
+        Me.Label8.TabIndex = 6
+        Me.Label8.Text = ":"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label9.Location = New System.Drawing.Point(114, 97)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(12, 17)
+        Me.Label9.TabIndex = 5
+        Me.Label9.Text = ":"
+        '
+        'wText
+        '
+        Me.wText.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.wText.Location = New System.Drawing.Point(132, 95)
+        Me.wText.Name = "wText"
+        Me.wText.Size = New System.Drawing.Size(177, 22)
+        Me.wText.TabIndex = 9
+        '
+        'hText
+        '
+        Me.hText.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.hText.Location = New System.Drawing.Point(132, 122)
+        Me.hText.Name = "hText"
+        Me.hText.Size = New System.Drawing.Size(177, 22)
+        Me.hText.TabIndex = 10
+        '
+        'BMIText
+        '
+        Me.BMIText.Enabled = False
+        Me.BMIText.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BMIText.Location = New System.Drawing.Point(132, 150)
+        Me.BMIText.Name = "BMIText"
+        Me.BMIText.Size = New System.Drawing.Size(177, 22)
+        Me.BMIText.TabIndex = 11
+        '
+        'BMICText
+        '
+        Me.BMICText.Enabled = False
+        Me.BMICText.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BMICText.Location = New System.Drawing.Point(132, 178)
+        Me.BMICText.Name = "BMICText"
+        Me.BMICText.Size = New System.Drawing.Size(177, 22)
+        Me.BMICText.TabIndex = 12
+        '
+        'CLEAR
+        '
+        Me.CLEAR.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CLEAR.Location = New System.Drawing.Point(172, 221)
+        Me.CLEAR.Name = "CLEAR"
+        Me.CLEAR.Size = New System.Drawing.Size(118, 54)
+        Me.CLEAR.TabIndex = 13
+        Me.CLEAR.Text = "CLEAR"
+        Me.CLEAR.UseVisualStyleBackColor = True
+        '
+        'btnadd
+        '
+        Me.btnadd.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnadd.Location = New System.Drawing.Point(45, 221)
+        Me.btnadd.Name = "btnadd"
+        Me.btnadd.Size = New System.Drawing.Size(118, 54)
+        Me.btnadd.TabIndex = 14
+        Me.btnadd.Text = "COMPUTE"
+        Me.btnadd.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(314, 124)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(32, 17)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "Cm"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label11.Location = New System.Drawing.Point(315, 97)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(26, 17)
+        Me.Label11.TabIndex = 15
+        Me.Label11.Text = "Kg"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(347, 309)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.btnadd)
+        Me.Controls.Add(Me.CLEAR)
+        Me.Controls.Add(Me.BMICText)
+        Me.Controls.Add(Me.BMIText)
+        Me.Controls.Add(Me.hText)
+        Me.Controls.Add(Me.wText)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "BMI Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents wText As TextBox
+    Friend WithEvents hText As TextBox
+    Friend WithEvents BMIText As TextBox
+    Friend WithEvents BMICText As TextBox
+    Friend WithEvents CLEAR As Button
+    Friend WithEvents btnadd As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+End Class
---
+++ Zamora, Allan Gel P. (BMI Calculator)/BMI Calculator/BMI Calculator/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private Sub btnadd_Click(sender As Object, e As EventArgs) Handles btnadd.Click
+
+        Dim weight As Double = Double.Parse(wText.Text)
+        Dim height As Double = Double.Parse(hText.Text)
+
+        Dim divideH As Double = height / 100
+        Dim BMI As Double = weight / (divideH * divideH)
+
+        BMIText.Text = BMI.ToString("0.00")
+
+        If (BMI < 15) Then
+            BMICText.Text = "Very Severely Underweight"
+        ElseIf (BMI < 16 And BMI <= 18) Then
+            BMICText.Text = "Severely Underweight"
+        ElseIf (BMI < 18 And BMI <= 25) Then
+            BMICText.Text = "Underweight"
+        ElseIf (BMI < 25 And BMI <= 30) Then
+            BMICText.Text = "Normal"
+        ElseIf (BMI < 30 And BMI <= 35) Then
+            BMICText.Text = "Overweight"
+        ElseIf (BMI < 35 And BMI <= 40) Then
+            BMICText.Text = "Moderately Obese"
+        ElseIf (BMI < 40) Then
+            BMICText.Text = "Severely Obese"
+        Else
+            BMICText.Text = "Very Severely Obese"
+        End If
+    End Sub
+
+    Private Sub CLEAR_Click(sender As Object, e As EventArgs) Handles CLEAR.Click
+        wText.Text = ""
+        hText.Text = ""
+        BMIText.Text = ""
+        BMICText.Text = ""
+
+
+
+    End Sub
+End Class
