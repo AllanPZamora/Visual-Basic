301ddcc1b673d722a9d8733498de3d738f04b914
Allan Zamora <gelgelacc1@gmail.com>
2023-09-30 00:55:44 +0800

    added functions to buttons

--- Zamora, Allan Gel P. (Calculator)/Calculator/Calculator/Form1.Designer.vb
+++ Zamora, Allan Gel P. (Calculator)/Calculator/Calculator/Form1.Designer.vb
@@ -22,13 +22,254 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.calcDisplay = New System.Windows.Forms.TextBox()
+        Me.Buttonequal = New System.Windows.Forms.Button()
+        Me.Buttondeci = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.Buttonmod = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ButtonAdd = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Buttonsub = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.buttonclear1 = New System.Windows.Forms.Button()
+        Me.buttonmulti = New System.Windows.Forms.Button()
+        Me.buttondivide = New System.Windows.Forms.Button()
+        Me.buttonclear = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'calcDisplay
+        '
+        Me.calcDisplay.Font = New System.Drawing.Font("Arial", 38.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.calcDisplay.Location = New System.Drawing.Point(12, 12)
+        Me.calcDisplay.Multiline = True
+        Me.calcDisplay.Name = "calcDisplay"
+        Me.calcDisplay.ReadOnly = True
+        Me.calcDisplay.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.calcDisplay.Size = New System.Drawing.Size(318, 67)
+        Me.calcDisplay.TabIndex = 0
+        '
+        'Buttonequal
+        '
+        Me.Buttonequal.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Buttonequal.Location = New System.Drawing.Point(255, 317)
+        Me.Buttonequal.Name = "Buttonequal"
+        Me.Buttonequal.Size = New System.Drawing.Size(75, 142)
+        Me.Buttonequal.TabIndex = 38
+        Me.Buttonequal.Text = "="
+        Me.Buttonequal.UseVisualStyleBackColor = True
+        '
+        'Buttondeci
+        '
+        Me.Buttondeci.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Buttondeci.Location = New System.Drawing.Point(175, 391)
+        Me.Buttondeci.Name = "Buttondeci"
+        Me.Buttondeci.Size = New System.Drawing.Size(75, 68)
+        Me.Buttondeci.TabIndex = 37
+        Me.Buttondeci.Text = "."
+        Me.Buttondeci.UseVisualStyleBackColor = True
+        '
+        'Button0
+        '
+        Me.Button0.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button0.Location = New System.Drawing.Point(94, 391)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(75, 68)
+        Me.Button0.TabIndex = 36
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'Buttonmod
+        '
+        Me.Buttonmod.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Buttonmod.Location = New System.Drawing.Point(13, 391)
+        Me.Buttonmod.Name = "Buttonmod"
+        Me.Buttonmod.Size = New System.Drawing.Size(75, 68)
+        Me.Buttonmod.TabIndex = 35
+        Me.Buttonmod.Text = "%"
+        Me.Buttonmod.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(175, 317)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 68)
+        Me.Button3.TabIndex = 34
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(94, 317)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 68)
+        Me.Button2.TabIndex = 33
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(13, 317)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 68)
+        Me.Button1.TabIndex = 32
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ButtonAdd
+        '
+        Me.ButtonAdd.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonAdd.Location = New System.Drawing.Point(255, 243)
+        Me.ButtonAdd.Name = "ButtonAdd"
+        Me.ButtonAdd.Size = New System.Drawing.Size(75, 68)
+        Me.ButtonAdd.TabIndex = 31
+        Me.ButtonAdd.Text = "+"
+        Me.ButtonAdd.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(174, 243)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 68)
+        Me.Button6.TabIndex = 30
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(93, 243)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 68)
+        Me.Button5.TabIndex = 29
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(12, 243)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 68)
+        Me.Button4.TabIndex = 28
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Buttonsub
+        '
+        Me.Buttonsub.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Buttonsub.Location = New System.Drawing.Point(256, 169)
+        Me.Buttonsub.Name = "Buttonsub"
+        Me.Buttonsub.Size = New System.Drawing.Size(75, 68)
+        Me.Buttonsub.TabIndex = 27
+        Me.Buttonsub.Text = "-"
+        Me.Buttonsub.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(175, 169)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(75, 68)
+        Me.Button9.TabIndex = 26
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(94, 169)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 68)
+        Me.Button8.TabIndex = 25
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(13, 169)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 68)
+        Me.Button7.TabIndex = 24
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'buttonclear1
+        '
+        Me.buttonclear1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonclear1.Location = New System.Drawing.Point(256, 95)
+        Me.buttonclear1.Name = "buttonclear1"
+        Me.buttonclear1.Size = New System.Drawing.Size(75, 68)
+        Me.buttonclear1.TabIndex = 23
+        Me.buttonclear1.Text = "⌫"
+        Me.buttonclear1.UseVisualStyleBackColor = True
+        '
+        'buttonmulti
+        '
+        Me.buttonmulti.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonmulti.Location = New System.Drawing.Point(175, 95)
+        Me.buttonmulti.Name = "buttonmulti"
+        Me.buttonmulti.Size = New System.Drawing.Size(75, 68)
+        Me.buttonmulti.TabIndex = 22
+        Me.buttonmulti.Text = "×"
+        Me.buttonmulti.UseVisualStyleBackColor = True
+        '
+        'buttondivide
+        '
+        Me.buttondivide.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttondivide.Location = New System.Drawing.Point(94, 95)
+        Me.buttondivide.Name = "buttondivide"
+        Me.buttondivide.Size = New System.Drawing.Size(75, 68)
+        Me.buttondivide.TabIndex = 21
+        Me.buttondivide.Text = "÷"
+        Me.buttondivide.UseVisualStyleBackColor = True
+        '
+        'buttonclear
+        '
+        Me.buttonclear.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonclear.Location = New System.Drawing.Point(13, 95)
+        Me.buttonclear.Name = "buttonclear"
+        Me.buttonclear.Size = New System.Drawing.Size(75, 68)
+        Me.buttonclear.TabIndex = 20
+        Me.buttonclear.Text = "C"
+        Me.buttonclear.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(320, 406)
+        Me.ClientSize = New System.Drawing.Size(343, 477)
+        Me.Controls.Add(Me.Buttonequal)
+        Me.Controls.Add(Me.Buttondeci)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Buttonmod)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ButtonAdd)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Buttonsub)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.buttonclear1)
+        Me.Controls.Add(Me.buttonmulti)
+        Me.Controls.Add(Me.buttondivide)
+        Me.Controls.Add(Me.buttonclear)
+        Me.Controls.Add(Me.calcDisplay)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -36,7 +277,28 @@ Partial Class Form1
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents calcDisplay As TextBox
+    Friend WithEvents Buttonequal As Button
+    Friend WithEvents Buttondeci As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents Buttonmod As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ButtonAdd As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Buttonsub As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents buttonclear1 As Button
+    Friend WithEvents buttonmulti As Button
+    Friend WithEvents buttondivide As Button
+    Friend WithEvents buttonclear As Button
 End Class
--- Zamora, Allan Gel P. (Calculator)/Calculator/Calculator/Form1.vb
+++ Zamora, Allan Gel P. (Calculator)/Calculator/Calculator/Form1.vb
@@ -1,3 +1,15 @@
 ﻿Public Class Form1
-
+    Private Sub numclicks(sender As Object, e As EventArgs) Handles Button9.Click, Button8.Click, Button7.Click, Button6.Click, Button5.Click, Button4.Click, Button3.Click, Button2.Click, Button1.Click, Button0.Click, Buttondeci.Click
+        Dim btn As Button = sender
+        If (calcDisplay.Text = "0") Then
+            calcDisplay.Clear()
+            calcDisplay.Text = btn.Text
+        ElseIf (btn.Text = ".") Then
+            If (Not calcDisplay.Text.Contains(".")) Then
+                calcDisplay.Text = calcDisplay.Text + btn.Text
+            End If
+        Else
+            calcDisplay.Text = calcDisplay.Text + btn.Text
+        End If
+    End Sub
 End Class
